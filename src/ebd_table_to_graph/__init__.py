842ced8a55d1f761a0b678c160d5fa7b4dfc44c8
konstantin <konstantin.klein@hochfrequenz.de>
2022-11-30 17:17:42 +0100

    ✨Introduce `get_all_nodes(table)` (#2)

--- src/ebd_table_to_graph/__init__.py
+++ src/ebd_table_to_graph/__init__.py
@@ -1,10 +1,26 @@
 """
 contains the conversion logic
 """
+from typing import List
+
 from networkx import DiGraph  # type:ignore[import]
 
-from ebd_table_to_graph.models.ebd_graph import EbdGraph, EbdGraphMetaData
+from ebd_table_to_graph.models.ebd_graph import DecisionNode, EbdGraph, EbdGraphMetaData, EbdGraphNodes, OutcomeNode
 from ebd_table_to_graph.models.ebd_table import EbdTable
+
+
+def get_all_nodes(table: EbdTable) -> List[EbdGraphNodes]:
+    """
+    Returns a list with all nodes from the table.
+    Nodes may both be actual EBD check outcome codes (e.g. "A55") but also points where decisions are made.
+    """
+    result: List[EbdGraphNodes] = []
+    for row in table.rows:
+        result.append(DecisionNode(question=row.description))  # the description of the row alone is always a node
+        for sub_row in row.sub_rows:
+            if sub_row.result_code is not None:
+                result.append(OutcomeNode(result_code=sub_row.result_code, note=sub_row.note))
+    return result
 
 
 def convert_table_to_graph(table: EbdTable) -> EbdGraph:

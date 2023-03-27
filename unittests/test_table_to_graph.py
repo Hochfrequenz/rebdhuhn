c6f6e62947d0bb11d0d748cba5008c936e35a03c
Nicola Söker <nicola.soeker@hochfrequenz.de>
2023-03-27 13:47:49 +0200

    better name for response file

--- unittests/test_table_to_graph.py
+++ unittests/test_table_to_graph.py
@@ -198,7 +198,7 @@ class TestEbdTableModels:
     def test_table_to_digraph_dot_with_watermark(self, add_background: bool, requests_mock):
         ebd_graph = convert_table_to_graph(table_e0003)
         dot_code = convert_graph_to_dot(ebd_graph)
-        with open(Path(__file__).parent / "test_files" / "E_0003.dot.svg", "r", encoding="utf-8") as infile:
+        with open(Path(__file__).parent / "test_files" / "E_0003_kroki_response.dot.svg", "r", encoding="utf-8") as infile:
             kroki_response_string: str = infile.read()
 
         requests_mock.post("https://kroki.io", text=kroki_response_string)

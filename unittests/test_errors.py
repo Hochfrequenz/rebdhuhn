f0905a7152667262121f142dc9eb4e364c613745
konstantin <konstantin.klein@hochfrequenz.de>
2023-07-19 14:55:39 +0200

    ✔ Add test case for `NotExactlyTwoOutgoingEdgesError` (FV2310 `E_0459`) (#101)

---
+++ unittests/test_errors.py
@@ -0,0 +1,22 @@
+import pytest  # type:ignore[import]
+
+from ebdtable2graph import convert_graph_to_plantuml, convert_table_to_graph
+from ebdtable2graph.models import EbdTable
+from ebdtable2graph.models.errors import NotExactlyTwoOutgoingEdgesError
+
+from .e0459 import table_e0459
+
+
+class TestErrors:
+    """
+    Test cases for various exceptions being raised. This can be the basis for future fixes/workarounds
+    """
+
+    @pytest.mark.parametrize("table", [pytest.param(table_e0459)])
+    def test_not_exactly_two_outgoing_edges_error(self, table: EbdTable):
+        """
+        Test the NotExactlyTwoOutgoingEdgesError
+        """
+        ebd_graph = convert_table_to_graph(table)
+        with pytest.raises(NotExactlyTwoOutgoingEdgesError):
+            _ = convert_graph_to_plantuml(ebd_graph)

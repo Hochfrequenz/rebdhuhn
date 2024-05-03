1110954d5ad2e1d9ba1a96173a0cb7adb51d3873
konstantin <konstantin.klein@hochfrequenz.de>
2024-05-03 10:46:23 +0200

    feat: Make kroki host configurable (but default to `locahost:8125`) (#208)

--- src/rebdhuhn/kroki.py
+++ src/rebdhuhn/kroki.py
@@ -13,9 +13,20 @@ class DotToSvgConverter(Protocol):
     a class that can convert dot to svg
     """
 
-    def convert_to_svg(self, dot_code: str) -> str:
+    def convert_dot_to_svg(self, dot_code: str) -> str:
         """
         convert the given dot to svg
+        """
+
+
+class PlantUmlToSvgConverter(Protocol):
+    """
+    a class that can convert plantuml to svg
+    """
+
+    def convert_plantuml_to_svg(self, plantuml_code: str) -> str:
+        """
+        convert the given plantuml code to svg
         """
 
 
@@ -25,11 +36,19 @@ class Kroki:
     A wrapper around any kroki request
     """
 
-    def convert_to_svg(self, dot_code: str) -> str:
+    def __init__(self, kroki_host: str = "http://localhost:8125") -> None:
+        """
+        initialize by providing the kroki host (e.g. https://kroki.io or http://localhost:8125...)
+        """
+        if not kroki_host:
+            raise ValueError("kroki_host must be provided")
+        self._host = kroki_host
+
+    def convert_dot_to_svg(self, dot_code: str) -> str:
         """
         returns the svg code as str
         """
-        url = "http://localhost:8125/"
+        url = self._host
         answer = requests.post(
             url,
             json={"diagram_source": dot_code, "diagram_type": "graphviz", "output_format": "svg"},
@@ -38,6 +57,23 @@ class Kroki:
         if answer.status_code != 200:
             raise ValueError(
                 f"Error while converting dot to svg: {answer.status_code}: {requests.codes[answer.status_code]}. "
+                f"{answer.text}"
+            )
+        return answer.text
+
+    def convert_plantuml_to_svg(self, plantuml_code: str) -> str:
+        """
+        returns the svg code as str
+        """
+        url = self._host
+        answer = requests.post(
+            url,
+            json={"diagram_source": plantuml_code, "diagram_type": "plantuml", "output_format": "svg"},
+            timeout=5,
+        )
+        if answer.status_code != 200:
+            raise ValueError(
+                f"Error while converting plantuml to svg: {answer.status_code}: {requests.codes[answer.status_code]}. "
                 f"{answer.text}"
             )
         return answer.text

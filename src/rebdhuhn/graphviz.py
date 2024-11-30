bf49f06b09f4f6f0875a54cd0f728a0ca18e3598
DeltaDaniel <139119540+DeltaDaniel@users.noreply.github.com>
2024-11-30 15:25:49 +0100

    puml, dot, svg output

--- src/rebdhuhn/graphviz.py
+++ src/rebdhuhn/graphviz.py
@@ -69,12 +69,17 @@ def _convert_outcome_node_to_dot(ebd_graph: EbdGraph, node: str, indent: str) ->
     """
     Convert an OutcomeNode to dot code
     """
-    formatted_label = (
-        f'<B>{ebd_graph.graph.nodes[node]["node"].result_code}</B><BR align="center"/>'
-        f'<FONT point-size="12">'
-        f'<U>Hinweis:</U><BR align="left"/>{_format_label(ebd_graph.graph.nodes[node]["node"].note)}<BR align="left"/>'
-        f"</FONT>"
-    )
+    is_outcome_without_code = ebd_graph.graph.nodes[node]["node"].result_code is None
+    formatted_label: str = ""
+    if not is_outcome_without_code:
+        formatted_label += f'<B>{ebd_graph.graph.nodes[node]["node"].result_code}</B><BR align="center"/>'
+    if ebd_graph.graph.nodes[node]["node"].note:
+        formatted_label = (
+            f'<FONT point-size="12">'
+            f'<U>Hinweis:</U><BR align="left"/>{_format_label(ebd_graph.graph.nodes[node]["node"].note)}<BR align="left"/>'
+            f"</FONT>"
+        )
+
     return (
         f'{indent}"{node}" '
         f'[margin="0.17,0.08", shape=box, style=filled, fillcolor="#cfb986", label=<{formatted_label}>];'

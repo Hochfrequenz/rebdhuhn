8f53c7930b57b0b502215e040ad59654870d02b7
konstantin <konstantin.klein@hochfrequenz.de>
2025-01-22 15:45:29 +0100

    add `fontname="Roboto, sans-serif"` to all nodes and edges in SVG (#334)
    still: the font has to be installed/availalble on the system where the SVG is displayed

--- src/rebdhuhn/graphviz.py
+++ src/rebdhuhn/graphviz.py
@@ -41,7 +41,7 @@ def _convert_start_node_to_dot(ebd_graph: EbdGraph, node: str, indent: str) -> s
     return (
         f'{indent}"{node}" '
         # pylint:disable=line-too-long
-        f'[margin="0.2,0.12", shape=box, style="filled,rounded", penwidth=0.0, fillcolor="#7a8da1", label=<{formatted_label}>];'
+        f'[margin="0.2,0.12", shape=box, style="filled,rounded", penwidth=0.0, fillcolor="#7a8da1", label=<{formatted_label}>, fontname="Roboto, sans-serif"];'
     )
 
 
@@ -56,7 +56,7 @@ def _convert_empty_node_to_dot(ebd_graph: EbdGraph, node: str, indent: str) -> s
     return (
         f'{indent}"{node}" '
         # pylint:disable=line-too-long
-        f'[margin="0.2,0.12", shape=box, style="filled,rounded", penwidth=0.0, fillcolor="#7a8da1", label=<{formatted_label}>];'
+        f'[margin="0.2,0.12", shape=box, style="filled,rounded", penwidth=0.0, fillcolor="#7a8da1", label=<{formatted_label}>, fontname="Roboto, sans-serif"];'
     )
 
 
@@ -65,7 +65,7 @@ def _convert_end_node_to_dot(node: str, indent: str) -> str:
     Convert an EndNode to dot code
     """
     # pylint:disable=line-too-long
-    return f'{indent}"{node}" [margin="0.2,0.12", shape=box, style="filled,rounded", penwidth=0.0, fillcolor="#7a8da1", label="Ende"];'
+    return f'{indent}"{node}" [margin="0.2,0.12", shape=box, style="filled,rounded", penwidth=0.0, fillcolor="#7a8da1", label="Ende", fontname="Roboto, sans-serif"];'
 
 
 def _convert_outcome_node_to_dot(ebd_graph: EbdGraph, node: str, indent: str) -> str:
@@ -81,7 +81,7 @@ def _convert_outcome_node_to_dot(ebd_graph: EbdGraph, node: str, indent: str) ->
     return (
         f'{indent}"{node}" '
         # pylint:disable=line-too-long
-        f'[margin="0.17,0.08", shape=box, style="filled,rounded", penwidth=0.0, fillcolor="#cfb986", label=<{formatted_label}>];'
+        f'[margin="0.17,0.08", shape=box, style="filled,rounded", penwidth=0.0, fillcolor="#cfb986", label=<{formatted_label}>, fontname="Roboto, sans-serif"];'
     )
 
 
@@ -96,7 +96,7 @@ def _convert_decision_node_to_dot(ebd_graph: EbdGraph, node: str, indent: str) -
     )
     return (
         f'{indent}"{node}" [margin="0.2,0.12", shape=box, style="filled,rounded", penwidth=0.0, fillcolor="#7aab8a", '
-        f"label=<{formatted_label}>];"
+        f'label=<{formatted_label}>, fontname="Roboto, sans-serif"];'
     )
 
 
@@ -135,14 +135,17 @@ def _convert_yes_edge_to_dot(node_src: str, node_target: str, indent: str) -> st
     """
     Converts a YesEdge to dot code
     """
-    return f'{indent}"{node_src}" -> "{node_target}" [label=<<B>JA</B>>, color="#7aab8a"];'
+    return (
+        f'{indent}"{node_src}" -> "{node_target}" [label=<<B>JA</B>>, color="#7aab8a", fontname="Roboto, sans-serif"];'
+    )
 
 
 def _convert_no_edge_to_dot(node_src: str, node_target: str, indent: str) -> str:
     """
     Converts a NoEdge to dot code
     """
-    return f'{indent}"{node_src}" -> "{node_target}" [label=<<B>NEIN</B>>, color="#7aab8a"];'
+    # pylint:disable=line-too-long
+    return f'{indent}"{node_src}" -> "{node_target}" [label=<<B>NEIN</B>>, color="#7aab8a", fontname="Roboto, sans-serif"];'
 
 
 def _convert_ebd_graph_edge_to_dot(node_src: str, node_target: str, indent: str) -> str:
@@ -204,7 +207,7 @@ def convert_graph_to_dot(ebd_graph: EbdGraph) -> str:
     if "Start" in nx_graph:
         assert len(nx_graph["Start"]) == 1, "Start node must have exactly one outgoing edge."
         dot_code += "\n".join(_convert_edges_to_dot(ebd_graph, ADD_INDENT)) + "\n"
-    dot_code += '\n    bgcolor="transparent";\n'
+    dot_code += '\n    bgcolor="transparent";\nfontname="Roboto, sans-serif";\n'
     return dot_code + "}"
 
 

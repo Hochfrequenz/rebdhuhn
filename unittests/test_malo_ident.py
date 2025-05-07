6aa2b49fa7d063c2f570b47ebdc4ecdea26d191b
Konstantin <konstantin.klein+github@hochfrequenz.de>
2025-05-07 23:43:08 +0200

    fix: don't break because of note not being `None` (support `E_0594`)
    fixes
    class 'rebdhuhn.models.errors.OutcomeCodeAndFurtherStepError
    Error while processing EBD E_0594: Found a sub_row with both a result code None and a reference to another decision node EbdCheckResult(result=True, subsequent_step_number='205'). This is not implemented yet.

---
+++ unittests/test_malo_ident.py
@@ -0,0 +1,35 @@
+"""
+a test file just for the famous most requests E_0594 (the MaLo Ident EBD)
+"""
+
+import json
+import os
+from pathlib import Path
+
+import cattrs
+import pytest
+from syrupy import SnapshotAssertion
+
+from rebdhuhn import convert_graph_to_dot, convert_table_to_graph
+from rebdhuhn.kroki import Kroki
+from rebdhuhn.models import EbdTable
+
+path_to_raw_table_json = Path(__file__).parent / "test_files" / "fv2504_ebd40b_e0594.json"
+assert path_to_raw_table_json.exists()
+
+
+@pytest.mark.snapshot
+def test_e0594_svg_creation(snapshot: SnapshotAssertion) -> None:
+    with open(path_to_raw_table_json, "r", encoding="utf-8") as f:
+        table_json = json.load(f)
+    e_0594_table = cattrs.structure(table_json, EbdTable)
+    assert e_0594_table.metadata.ebd_code == "E_0594"
+    graph = convert_table_to_graph(e_0594_table)
+    dot_code = convert_graph_to_dot(graph)
+    svg_code = Kroki().convert_dot_to_svg(dot_code)
+    os.makedirs(Path(__file__).parent / "output", exist_ok=True)
+    with open(
+        Path(__file__).parent / "output" / f"table_dot_svg_{graph.metadata.ebd_code}.svg", "w+", encoding="utf-8"
+    ) as svg_file:
+        svg_file.write(svg_code)
+    assert dot_code == snapshot(name=f"table_dot_svg_{graph.metadata.ebd_code}")
